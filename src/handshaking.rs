b262f3b91f21432b1a28c368841ad974493e8cc6
Erik-Sanjaya <erik.sanjaya25@gmail.com>
2023-06-24 17:29:48 +0700

    feat: read handshake

---
+++ src/handshaking.rs
@@ -0,0 +1,111 @@
+use std::{io::Cursor, string::FromUtf8Error};
+
+use thiserror::Error;
+use tokio::io::AsyncReadExt;
+
+use crate::data_types::{VarInt, VarIntError};
+
+#[derive(Debug)]
+pub struct Handshaking {
+    packet_id: VarInt,
+    protocol_version: VarInt,
+    server_address: String,
+    server_port: u16,
+    next_state: HandshakingNextState,
+}
+
+#[derive(Debug)]
+enum HandshakingNextState {
+    Status = 1,
+    Login = 2,
+}
+
+#[derive(Debug, Error)]
+pub enum HandshakingError {
+    #[error("Packet length is invalid: {0}")]
+    Length(#[source] VarIntError),
+    #[error("Packet id is invalid: {0}")]
+    PacketId(#[source] VarIntError),
+    #[error("Protocol version is invalid: {0}")]
+    ProtocolVersion(#[source] VarIntError),
+    #[error("Error with server address: {0}")]
+    ServerAddress(#[source] ServerAddressError),
+    #[error("Server port is invalid: {0}")]
+    ServerPort(#[source] std::io::Error),
+    #[error("Error with next state: {0}")]
+    NextState(#[source] NextStateError),
+}
+
+#[derive(Debug, Error)]
+pub enum ServerAddressError {
+    #[error("Invalid server address length: {0}")]
+    Length(#[source] VarIntError),
+    #[error("Server address is missing bytes: {0}")]
+    MissingBytes(#[source] std::io::Error),
+    #[error("Server address is invalid: {0}")]
+    Parsing(#[source] FromUtf8Error),
+}
+
+#[derive(Debug, Error)]
+pub enum NextStateError {
+    #[error("Packet length is invalid: {0}")]
+    Parse(#[source] VarIntError),
+    #[error("There is no next state option: {0}")]
+    InvalidType(i32),
+}
+
+impl Handshaking {
+    pub async fn read(cursor: &mut Cursor<&[u8]>) -> Result<Self, HandshakingError> {
+        let _length = VarInt::read(cursor)
+            .await
+            .map_err(HandshakingError::Length)?;
+
+        let packet_id = VarInt::read(cursor)
+            .await
+            .map_err(HandshakingError::PacketId)?;
+
+        let protocol_version = VarInt::read(cursor)
+            .await
+            .map_err(HandshakingError::ProtocolVersion)?;
+
+        let server_address = {
+            let server_addr_len = VarInt::read(cursor)
+                .await
+                .map_err(ServerAddressError::Length)
+                .map_err(HandshakingError::ServerAddress)?;
+            let mut server_addr_buffer = vec![0; server_addr_len.0 as usize];
+            cursor
+                .read_exact(&mut server_addr_buffer)
+                .await
+                .map_err(ServerAddressError::MissingBytes)
+                .map_err(HandshakingError::ServerAddress)?;
+
+            String::from_utf8(server_addr_buffer)
+                .map_err(ServerAddressError::Parsing)
+                .map_err(HandshakingError::ServerAddress)?
+        };
+
+        let server_port = cursor
+            .read_u16()
+            .await
+            .map_err(HandshakingError::ServerPort)?;
+
+        let next_state = match VarInt::read(cursor)
+            .await
+            .map_err(NextStateError::Parse)
+            .map_err(HandshakingError::NextState)?
+        {
+            VarInt(1) => HandshakingNextState::Status,
+            VarInt(2) => HandshakingNextState::Login,
+            VarInt(n) => return Err(HandshakingError::NextState(NextStateError::InvalidType(n))),
+        };
+
+        Ok(Handshaking {
+            packet_id,
+            protocol_version,
+            server_address,
+            server_port,
+            next_state,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod data_types;
+mod handshaking;
 
 use std::io::Cursor;
 
 use data_types::{VarInt, VarIntError};
+use serde_json::json;
 use thiserror::Error;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
@@ -10,6 +12,10 @@ use tokio::{
 };
 use tracing::{debug, error, info};
 
+use crate::handshaking::Handshaking;
+
+// This is mostly only used for debugging. Although I doubt it'd be done that
+// many times using this. But I'll keep it anyway
 #[derive(Debug)]
 struct UncompressedPacket<'d> {
     length: VarInt,
@@ -44,7 +50,7 @@ async fn into_uncompressed_dirty(
     })
 }
 
-async fn handle_socket(mut stream: TcpStream) -> anyhow::Result<()> {
+async fn stream_into_vec(stream: &mut TcpStream) -> anyhow::Result<Vec<u8>> {
     let mut length_buffer = [0; 5];
     stream.peek(&mut length_buffer[..]).await?;
     let mut length_cursor = Cursor::new(&length_buffer[..]);
@@ -55,15 +61,66 @@ async fn handle_socket(mut stream: TcpStream) -> anyhow::Result<()> {
         Ok(_) => (),
         Err(e) => {
             error!("{:?}", e);
-            info!("Buffer dump: {:?}", buffer);
+            debug!("Buffer dump: {:?}", buffer);
         }
     };
-    let packet = into_uncompressed_dirty(&buffer[..]).await;
+
+    Ok(buffer)
+}
+
+async fn handle_socket(mut stream: TcpStream) -> anyhow::Result<()> {
+    let buffer = stream_into_vec(&mut stream).await?;
+    let mut cursor = Cursor::new(buffer.as_slice());
+    let handshake = Handshaking::read(&mut cursor).await?;
+    // let packet = into_uncompressed_dirty(&buffer[..]).await;
 
     debug!("{:?}", buffer);
-    debug!("{:?}", packet);
+    debug!("{:?}", handshake);
 
-    stream.write_all(&buffer[..]).await?;
+    // TODO: implement all of these into actual struct.
+    let mut response_buffer_test = vec![];
+
+    let dummy_json_string = json!({
+        "version": {
+            "name": "1",
+            "protocol": 764
+        },
+        "players": {
+            "max": 100,
+            "online": 5,
+            "sample": [
+                {
+                    "name": "thinkofdeath",
+                    "id": "4566e69f-c907-48ee-8d71-d7ba5aa00d20"
+                }
+            ]
+        },
+        "description": {
+            "text": "Hello world"
+        },
+        "favicon": "data:image/png;base64,<data>",
+        "enforcesSecureChat": true,
+        "previewsChat": true
+    })
+    .to_string();
+
+    // packet length, 3 is hardcoded from 1 byte for packet id and 2 bytes for
+    // string length (VarInt)
+    VarInt(3 + dummy_json_string.len() as i32)
+        .write(&mut response_buffer_test)
+        .await;
+
+    // packet id
+    VarInt(0).write(&mut response_buffer_test).await;
+
+    VarInt(dummy_json_string.len() as i32)
+        .write(&mut response_buffer_test)
+        .await;
+
+    response_buffer_test.extend_from_slice(dummy_json_string.as_bytes());
+
+    stream.write_all(&response_buffer_test).await?;
+    debug!("WROTE DUMMY JSON RESPONSE: {:?}", response_buffer_test);
     Ok(())
 }
 
@@ -77,13 +134,12 @@ async fn main() -> anyhow::Result<()> {
     tracing::subscriber::set_global_default(subscriber)?;
 
     loop {
-        let (stream, addr) = listener.accept().await?;
+        let (stream, _addr) = listener.accept().await?;
         tokio::spawn(async move {
-            info!("addr: {:?} | Tcp: {:?}", addr, stream);
-            match handle_socket(stream).await {
-                Ok(_) => (),
-                Err(e) => error!("{:?}", e),
-            };
+            info!("{:?}", stream);
+            let _ = handle_socket(stream)
+                .await
+                .map_err(|err| error!("{:?}", err));
         })
         .await?;
     }
